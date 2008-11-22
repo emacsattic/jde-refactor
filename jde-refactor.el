233d89dfc31da9289ed8f1861311ca8f209348cd
Espen Wiborg <espenhw@grumblesmurf.org>
2008-11-22 21:46:55 +0100

    Handle renaming method parameters from method declaration.

--- jde-refactor.el
+++ jde-refactor.el
@@ -1,8 +1,6 @@
 (defun jde-refactor-rename (to-name &optional query)
   "Rename the thing at point to TO-NAME. If query (the prefix
-argument) is non-NIL, query each renaming.  For method arguments,
-it currently only works from within the method body; i.e. not
-when point is in the method header."
+argument) is non-NIL, query each renaming."
   ;; TODO: Handle methods (at least in-class uses)
   ;; TODO: Deal with name conflicts
   ;; TODO-MAYBE: Deal with non-private members by renaming all users
@@ -19,10 +17,18 @@ when point is in the method header."
              (and (string= (car pair) "")
                   (jde-parse-find-declaration-of thing-of-interest))))
         (when declaration-pos
-          (let ((boundaries (jde-refactor-block-boundaries)))
+          (let ((boundaries
+                 (if (jde-refactor-parameter-of-current-method-p thing-of-interest)
+                     (cons (semantic-tag-start (semantic-current-tag))
+                           (semantic-tag-end (semantic-current-tag)))
+                   (jde-refactor-block-boundaries))))
             (funcall (if query 'query-replace 'replace-string)
                      thing-of-interest to-name
                      t declaration-pos (cdr boundaries))))))))
+
+(defun jde-refactor-parameter-of-current-method-p (thing)
+  (find thing (semantic-tag-function-arguments (semantic-current-tag-of-class 'function))
+        :key #'car :test #'string=))
 
 (defun jde-refactor-block-boundaries ()
   "Return a pair (START . END) denoting the boundaries of the
